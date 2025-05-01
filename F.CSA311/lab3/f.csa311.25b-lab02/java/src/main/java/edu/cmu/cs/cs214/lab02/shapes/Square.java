23c6eb6e26fc87850d818191507f109066f765ee
Duureeee <167115601+Duureeee@users.noreply.github.com>
2025-05-02 05:58:10 +0800

    Add files via upload

---
+++ F.CSA311/lab3/f.csa311.25b-lab02/java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Square.java
@@ -0,0 +1,19 @@
+package edu.cmu.cs.cs214.lab02.shapes;
+
+public class Square implements Shape {
+    private final double sideLen;
+
+    public Square(double sideLen) {
+        this.sideLen = sideLen;
+    }
+
+    @Override
+    public double getArea() {
+        return sideLen * sideLen;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Square");
+    }
+}
